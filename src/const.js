e20431ca2a7fd2c594a0f3b289a2f8380e8cd9c7
kentaro_matsutani <kentaro_matsutani@bita.jp>
2020-08-19 16:28:11 +0900

    feat: 残業一括申請スクリプト追加

---
+++ src/const.js
@@ -0,0 +1,28 @@
+export const BASE_URL = "https://attendance.moneyforward.com";
+
+export const PATH = {
+  SIGN_IN: `${BASE_URL}/employee_session/new`,
+  STAMP_TABLE: `${BASE_URL}/my_page/attendances`,
+};
+
+export const ELEMENTS = {
+  SIGN_IN: {
+    FORM_COMPANY_ID: "#employee_session_form_office_account_name",
+    FORM_EMAIL: "#employee_session_form_account_name_or_email",
+    FORM_PASSWORD: "#employee_session_form_password",
+    BUTTON_SUBMIT: "input[type=submit]",
+  },
+  STAMP_TABLE: {
+    TABLE_ROW: ".attendance-table-row-",
+    TABLE_DATA_HOLIDAY: ".column-classification",
+    TABLE_DATA_OVERTIME: ".column-status",
+    ANCHOR_OVERTIME_REQUEST:
+      'a[href*="/my_page/workflow_requests/late_overtime_works"]',
+  },
+  OVERTIME_REQUEST: {
+    FORM_OVERTIME:
+      "workflow_request_workflow_request_content_late_overtime_work_attributes_workable_time",
+    FORM_COMMENT: "#workflow_request_comment",
+    BUTTON_SUBMIT: 'input[type="submit"]',
+  },
+};
---
+++ src/stamp.js
@@ -0,0 +1,65 @@
+import { COMPANY_ID, EMAIL, PASSWORD, OVERTIME, COMMENT } from "../env.js";
+import { PATH, ELEMENTS, BASE_URL } from "./const.js";
+import puppeteer from "puppeteer";
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // sign in
+  await page.goto(PATH.SIGN_IN);
+  await page.type(ELEMENTS.SIGN_IN.FORM_COMPANY_ID, COMPANY_ID);
+  await page.type(ELEMENTS.SIGN_IN.FORM_EMAIL, EMAIL);
+  await page.type(ELEMENTS.SIGN_IN.FORM_PASSWORD, PASSWORD);
+  await Promise.all([
+    page.click(ELEMENTS.SIGN_IN.BUTTON_SUBMIT),
+    page.waitForNavigation(),
+  ]);
+
+  // stamp table
+  await page.goto(PATH.STAMP_TABLE);
+
+  const linkList = await page.evaluate(
+    ({ ELEMENTS, BASE_URL }) => {
+      const rows = [
+        ...document.querySelectorAll(ELEMENTS.STAMP_TABLE.TABLE_ROW),
+      ];
+      const isTargetDate = (tableRow) => {
+        const isWorkDay = !tableRow
+          .querySelector(ELEMENTS.STAMP_TABLE.TABLE_DATA_HOLIDAY)
+          .innerHTML.match(/休日/);
+        const unrequested = !tableRow
+          .querySelector(ELEMENTS.STAMP_TABLE.TABLE_DATA_OVERTIME)
+          .innerHTML.match(/残業/);
+        return isWorkDay && unrequested;
+      };
+      const targetRows = rows.filter(isTargetDate);
+      return targetRows.map((row) => {
+        const overtimeRequestPagePath = row
+          .querySelector(ELEMENTS.STAMP_TABLE.ANCHOR_OVERTIME_REQUEST)
+          .getAttribute("href");
+        return `${BASE_URL}${overtimeRequestPagePath}`;
+      });
+    },
+    { ELEMENTS, BASE_URL }
+  );
+
+  // overtime request
+  for (const link of linkList) {
+    await page.goto(link);
+    await page.evaluate(
+      ({ ELEMENTS, OVERTIME }) => {
+        const overTimeForm = document.getElementById(
+          ELEMENTS.OVERTIME_REQUEST.FORM_OVERTIME
+        );
+        overTimeForm.value = OVERTIME;
+      },
+      { ELEMENTS, OVERTIME }
+    );
+    await page.type(ELEMENTS.OVERTIME_REQUEST.FORM_COMMENT, COMMENT);
+    await page.click(ELEMENTS.OVERTIME_REQUEST.BUTTON_SUBMIT);
+    await page.waitForNavigation();
+  }
+
+  await browser.close()
+})();
